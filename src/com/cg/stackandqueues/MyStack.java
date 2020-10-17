8884c30cae45346b568ec88b4d105973cdc673c4
AV-NASH <avinash21213434@gmail.com>
2020-10-18 00:06:46 +0530

    Stack created and added method to push by implementing linked list

--- src/com/cg/stackandqueues/MyStack.java
+++ src/com/cg/stackandqueues/MyStack.java
@@ -1,6 +1,14 @@
 package com.cg.stackandqueues;
 
-public class MyStack   {
+public class MyStack <E>  {
+    MyLinkedList myLinkedList=new MyLinkedList();
 
+    public void push(E data){
+        myLinkedList.add(data);
     }
+
+    public void printStack(){
+        myLinkedList.printList();
+    }
+}
 
--- src/com/cg/stackandqueues/StackAndQueueOperations.java
+++ src/com/cg/stackandqueues/StackAndQueueOperations.java
@@ -3,5 +3,10 @@ package com.cg.stackandqueues;
 public class StackAndQueueOperations {
     public static void main(String[] args) {
         System.out.println("Welcome to stack and queue operations");
+        MyStack myStack=new MyStack();
+        myStack.push(70);
+        myStack.push(30);
+        myStack.push(56);
+        myStack.printStack();
     }
 }
