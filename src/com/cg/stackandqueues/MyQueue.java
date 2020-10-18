4cc537506cd52f2b5038ade7f88a600a91af43de
AV-NASH <avinash21213434@gmail.com>
2020-10-18 13:29:23 +0530

    created queue class by implementing linked list and added enqueue method

--- src/com/cg/stackandqueues/MyQueue.java
+++ src/com/cg/stackandqueues/MyQueue.java
@@ -1,4 +1,18 @@
 package com.cg.stackandqueues;
 
-public class MyQueue  {
+import java.util.LinkedList;
+
+public class MyQueue<E>  {
+    MyLinkedList myLinkedList=new MyLinkedList();
+
+    public void enqueue(E data){
+        myLinkedList.append(data);
+    }
+
+    public void printQueue(){
+        if(myLinkedList.head==null)
+            System.out.println("Stack is empty");
+        else
+            myLinkedList.printList();
+    }
 }
--- src/com/cg/stackandqueues/StackAndQueueOperations.java
+++ src/com/cg/stackandqueues/StackAndQueueOperations.java
@@ -3,6 +3,7 @@ package com.cg.stackandqueues;
 public class StackAndQueueOperations {
     public static void main(String[] args) {
         System.out.println("Welcome to stack and queue operations");
+        System.out.println("Stack Operations");
         MyStack myStack=new MyStack();
         myStack.push(70);
         myStack.push(30);
@@ -11,9 +12,13 @@ public class StackAndQueueOperations {
         System.out.println("----");
         System.out.println("Element at top of stact is.."+myStack.peek()+"\nAfter pop");
         myStack.pop();
-        myStack.pop();
-        myStack.pop();
-        myStack.pop();
         myStack.printStack();
+
+        System.out.println("x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x\n"+"Queue operations");
+        MyQueue myQueue=new MyQueue();
+        myQueue.enqueue(56);
+        myQueue.enqueue(30);
+        myQueue.enqueue(70);
+        myQueue.printQueue();
     }
 }
